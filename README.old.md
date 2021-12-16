d1420e74e0aa76895a59045bb81a86ccb6a6ca9b
joshcamp503 <joshcamp503@gmail.com>
2021-12-15 22:38:16 -0800

    initial commit, basic react framework initialized, tinkered with navbar

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# portfolio
+A showcase of my web development skills, with links to completed projects and github repositories
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return ( 
+    <nav>
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">RESUME</a></li>
+        <li><a href="#">PROJECTS</a></li>
+        <li><a href="#">ABOUT</a></li>
+      </ul>
+    </nav>
+  );
+}
+ 
+export default Navbar;
